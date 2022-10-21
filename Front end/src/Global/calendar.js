0f14ee3f79812a7d541755f3630e326eaaf58be3
Gschmit <114729869+Gschmit@users.noreply.github.com>
2022-10-21 10:01:08 +0200

    Create calendar.js

---
+++ Front end/src/Global/calendar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './global.css';
+
+import { Calendar, globalizeLocalizer } from 'react-big-calendar'
+import globalize from 'globalize'
+
+const localizer = globalizeLocalizer(globalize)
+
+const MyCalendar = props => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
+
+/*
+class Calendar extends React.Component{
+    constructor(props){
+        super(props)
+        state={
+            today: new Date(),
+            activeDate: "activeDate" in this.props ? this.props.activeDate : new Date(),
+            meetings: "meetings" in this.props ? this.props.meetings : [],
+        }
+    }
+    render()
+} */
+
+export default MyCalendar
